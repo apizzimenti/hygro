8326c41b7c5f07f5b417b7e906944efe6c9251c9
Anthony Pizzimenti <pizzimentianthony@gmail.com>
2018-03-16 03:59:19 -0500

    Change the data saved and returned.

--- data.py
+++ data.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 import Adafruit_DHT
-import time
+import datetime
 import json
 
 def now():
     humidity, temp = Adafruit_DHT.read_retry(11, 4)
-    ltime = time.time()
+    ltime = datetime.datetime.now()
     data = {
-        "humidity": humidity,
-        "temperature": temp,
-        "time": int(ltime)
+        "humidity": str(int(humidity)) + "%",
+        "temperature": str((1.8 * temp) + 32) + u"\u00b0",
+        "time": str(ltime)
     }
 
     return json.dumps(data) 
